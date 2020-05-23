60e91043578261e14025f5720b89034e0ffa7d8a
dev-yoshiki <ben.nakagawa01@gmail.com>
2020-05-24 08:49:34 +0900

    Removed unnecessary line

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from primely.controller import controller
+from src.primely.controller import controller
 
-def execute():
-    controller.paycheck_analysis()
 
 if __name__ == "__main__":
     controller.paycheck_analysis()
