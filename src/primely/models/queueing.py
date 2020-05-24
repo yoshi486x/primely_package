cba8f03c53c09f2776687acef8f13e5b77f00655
dev-yoshiki <ben.nakagawa01@gmail.com>
2020-05-24 16:15:33 +0900

    Modified class to make it a simple func

--- src/primely/models/queueing.py
+++ src/primely/models/queueing.py
@@ -1,52 +1,42 @@
-import configparser
 import os
 import pathlib
 
-try:
-    from primely.views import utils
-except:
-    from primelyr.primely.views import utils
+from primely.views import utils
 
-# import global parameters from config.ini
-config = configparser.ConfigParser()
-config.read('config.ini')
-PDF_DIR_PATH = config['STORAGE']['PDF']
+PDF_DIR_PATH = 'data/input'
 
 
-class InputQueue(object):
-    def __init__(self, base_dir=None, all_files=None, pdf_files=None):
-        if not base_dir:
-            base_dir = utils.get_base_dir_path(__file__)
-        self.base_dir = base_dir
-        self.all_files = all_files
-        if not pdf_files:
-            pdf_files = self.load_pdf_filenames()
-        self.pdf_files = pdf_files
-    
-    def load_pdf_filenames(self):
-        """List all pdf files in designated directory"""
+def extract_filenames(self, all_files=[]):
+    """Extract filename without suffix"""
 
-        pdf_full_dir_path = pathlib.Path(self.base_dir, PDF_DIR_PATH)
-        return os.listdir(pdf_full_dir_path)
+    for item in os.listdir(PDF_DIR_PATH):
+        filename, _ = os.path.splitext(item)
+        all_files.append(filename)
 
-    def extract_filenames(self, all_files=[]):
-        """Extract filename without suffix"""
+    return sorted(all_files)
 
-        for item in self.pdf_files:
-            filename, _ = os.path.splitext(item)
-            all_files.append(filename)
-        self.all_files = sorted(all_files)
+# class InputQueue(object):
+    # def __init__(self, all_files=None):
 
-    def get_filename_list(self):
-        return self.all_files
+    # def extract_filenames(self, all_files=[]):
+    #     """Extract filename without suffix"""
+
+    #     for item in os.listdir(PDF_DIR_PATH):
+    #         filename, _ = os.path.splitext(item)
+    #         all_files.append(filename)
+    #     # self.all_files = sorted(all_files)
+    #     return sorted(all_files)
+
+    # def get_filename_list(self):
+    #     return self.all_files
 
 
-def main():
-    # inputQueue = InputQueue()
-    # filenames = inputQueue.load_pdf_filenames()
-    # print('pdf_files:', inputQueue.pdf_files)
-    # print('all_files:', inputQueue.all_files)
-    pass
 
 if __name__ == "__main__":
-    main()
+    inputQueue = InputQueue()
+    # inputQueue.load_pdf_filenames()
+    # inputQueue.extract_filenames()
+    # print('pdf_files:', inputQueue.pdf_files)
+    # print('all_files:', inputQueue.all_files)
+
+    print(inputQueue.extract_filenames())
